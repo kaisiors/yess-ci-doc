b5d8843ba0882209e51d72cb854e02a413e9ff58
Muhammad Kautsar <kautsarmuhammad19@gmail.com>
2026-01-13 16:27:52 +0700

    add config basePath

--- zudoku.config.tsx
+++ zudoku.config.tsx
@@ -1,6 +1,7 @@
 import type { ZudokuConfig } from "zudoku";
 
 const config: ZudokuConfig = {
+  basePath: "/yess-ci-doc",
   site: {
     logo: {
       src: { light: "/logo-light.svg", dark: "/logo-dark.svg" },
